0a86e271f748378c8fc3eb6d3b83e9d2952c6864
tnandate <nandate.takuya@moneyforward.co.jp>
2021-07-25 02:06:39 +0900

    Fix spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-require "four_arithmetic_operations"
+require 'four_arithmetic_operations'
+require 'pry'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
